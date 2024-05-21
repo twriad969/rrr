f4bc3a1996eb3d934c024f9278b965cbe4f0b440
twriad969 <157061362+twriad969@users.noreply.github.com>
2024-05-22 02:31:31 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { 
+    handleStartCommand, 
+    handleMessage, 
+    handleCallbackQuery, 
+    checkSubscription 
+} = require('./worker');
+const { 
+    handleCheckApiCommand, 
+    handleRotateApiCommand, 
+    handleStatsCommand 
+} = require('./command');
+
+const token = '7198843527:AAHoJwSWIAfvI8K8BaS6YmgIwZbFJUoGggM';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const text = msg.text;
+
+    if (text.startsWith('/check-api')) {
+        await handleCheckApiCommand(bot, chatId);
+    } else if (text.startsWith('/check-api=')) {
+        const apiName = text.split('=')[1];
+        await handleRotateApiCommand(bot, chatId, apiName);
+    } else if (text.startsWith('/stats')) {
+        await handleStatsCommand(bot, chatId);
+    } else {
+        await handleMessage(bot, chatId, userId, text);
+    }
+});
+
+bot.onText(/\/start$/, async (msg) => {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    await handleStartCommand(bot, chatId, userId);
+});
+
+bot.onText(/\/start (.+)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const uniqueCode = match[1];
+    await handleStartCommand(bot, chatId, userId, uniqueCode);
+});
+
+bot.on('callback_query', async (callbackQuery) => {
+    const msg = callbackQuery.message;
+    const chatId = msg.chat.id;
+    const userId = callbackQuery.from.id;
+
+    await handleCallbackQuery(bot, chatId, userId, callbackQuery.data);
+});
+
+console.log('Bot is running...');
